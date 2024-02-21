60270545ebea3f7b6a112eaffec73bafc7413504
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-02-21 11:03:25 -0600

    Add tests for shell and serverCommands and update previous ones to reflect changes

--- src/test/commands/stackCommands.test.ts
+++ src/test/commands/stackCommands.test.ts
@@ -1,18 +1,22 @@
 import * as assert from 'assert';
 import { getActiveStack } from '../../commands/stackCommands';
 import { Shell } from '../../utils/shell';
-import { parseActiveStackName } from '../../utils/helpers';
 
 class MockShell extends Shell {
-  static execCLICommand(): Promise<string> {
-    return Promise.resolve("The global active stack is: 'default'");
+  public async runPythonScript(scriptPath: string, args: string[] = []): Promise<any> {
+    if (scriptPath.includes('get_active_stack.py')) {
+      return JSON.stringify({
+        name: 'default'
+      });
+    }
+    throw new Error('Unexpected script path');
   }
 }
 
 suite('Stack Commands Test Suite', () => {
   test('getActiveStack correctly fetches the active stack name', async () => {
-    const cliOutput = await getActiveStack(MockShell.execCLICommand);
-    const activeStack = parseActiveStackName(cliOutput);
-    assert.strictEqual(activeStack, 'default', 'getActiveStack did not return the expected stack name');
+    const mockShell = new MockShell();
+    const activeStackName = await getActiveStack(mockShell);
+    assert.strictEqual(activeStackName, 'default', 'getActiveStack did not return the expected stack name');
   });
 });
