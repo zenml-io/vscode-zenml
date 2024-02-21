ebd5b6776db2d6a118c32296f6c0fee472845f0c
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-02-21 11:04:48 -0600

    Register server command in extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,28 @@
 import * as vscode from 'vscode';
 import { ZenMLStatusBar } from './views/statusBar';
+import { Shell } from './utils/shell';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('ZenML extension is now active!');
 
-	const zenMLStatusBar = ZenMLStatusBar.getInstance();
+	// Initialize a Shell instance (for making calls to the python client)
+	const shell = new Shell();
 
-	const refreshCommand = vscode.commands.registerCommand('zenml.showActiveStack', () => {
-		zenMLStatusBar.updateStatusBar();
+	// Pass the Shell instance to the status bar
+	const zenMLStatusBar = ZenMLStatusBar.getInstance(shell);
+
+	// Register Commands
+	const updateActiveStackCommand = vscode.commands.registerCommand('zenml.updateActiveStack', async () => {
+		await zenMLStatusBar.updateActiveStack();
 	});
 
-	context.subscriptions.push(refreshCommand);
+	const updateServerStatusCommand = vscode.commands.registerCommand('zenml.updateServerStatus', async () => {
+		await zenMLStatusBar.checkServerStatus();
+	});
+
+	// Add the commands to the context's subscriptions
+	context.subscriptions.push(updateActiveStackCommand);
+	context.subscriptions.push(updateServerStatusCommand);
 }
 
 export function deactivate() { }
