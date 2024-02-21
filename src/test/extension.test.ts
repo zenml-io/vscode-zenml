9508f57b4aefa68e30ed5e3c12d7b0f8109fbe16
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-02-21 11:03:25 -0600

    Add tests for shell and serverCommands and update previous ones to reflect changes

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,11 +3,11 @@ import * as vscode from 'vscode';
 
 suite('Extension Test Suite', () => {
 	test('Extension should be present', () => {
-		assert.ok(vscode.extensions.getExtension('zenml.zenml'));
+		assert.ok(vscode.extensions.getExtension('zenml-io.zenml'));
 	});
 
 	test('Extension should activate', async () => {
-		const extension = vscode.extensions.getExtension('zenml.zenml');
+		const extension = vscode.extensions.getExtension('zenml-io.zenml');
 		if (extension) {
 			await extension.activate();
 			assert.ok(extension.isActive, 'Extension did not activate as expected.');
