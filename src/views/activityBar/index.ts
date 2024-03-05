5d6099cf0edf767c2f91bad465b1981be2b7d0d4
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-03-04 19:59:40 -0600

    Add exports for the PipelineDataProvider and PipelineTreeItems

--- src/views/activityBar/index.ts
+++ src/views/activityBar/index.ts
@@ -2,3 +2,5 @@
 export * from "./stackView/StackDataProvider";
 export * from "./serverView/ServerDataProvider";
 export * from "./stackView/StackTreeItems";
+export * from "./pipelineView/PipelineDataProvider";
+export * from "./pipelineView/PipelineTreeItems";
