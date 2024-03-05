0732a3102d350dc38696bbd27d7ea2787210315d
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-03-04 19:57:26 -0600

    Update types for Stack and Server to reflect latest structure, and add PipelineTypes

--- src/types/ServerTypes.ts
+++ src/types/ServerTypes.ts
@@ -1,11 +1,23 @@
-/**
- * Interface representing the status of a server, including connectivity and optional details,
- * such as host and port if connected, or storeType and storeUrl if disconnected.
- */
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.See the License for the specific language governing
+// permissions and limitations under the License.
 export interface ServerStatus {
   isConnected: boolean;
-  host?: string;
-  port?: number;
-  storeType?: string;
-  storeUrl?: string;
+  url: string;
+  id: string;
+  version: string;
+  debug: boolean;
+  deployment_type: string;
+  database_type: string;
+  secrets_store_type: string;
+  auth_scheme: string;
 }
