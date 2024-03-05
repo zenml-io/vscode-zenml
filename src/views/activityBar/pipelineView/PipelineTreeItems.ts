855e055ce0f742c9462cba90167bfaed21a3df6d
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-03-04 18:58:13 -0600

    Introduce pipeline view to display pipeline runs

---
+++ src/views/activityBar/pipelineView/PipelineTreeItems.ts
@@ -0,0 +1,55 @@
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.See the License for the specific language governing
+// permissions and limitations under the License.
+import * as vscode from 'vscode';
+import { PipelineRun } from '../../../types/PipelineTypes';
+
+/**
+ * Represents a pipeline run in the VS Code tree view.
+ * Displays its name, version and status.
+ */
+export class PipelineTreeItem extends vscode.TreeItem {
+  public children: PipelineRunTreeItem[] | undefined;
+
+  constructor(
+    public readonly run: PipelineRun,
+    children?: PipelineRunTreeItem[]
+  ) {
+    super(
+      run.name,
+      children === undefined
+        ? vscode.TreeItemCollapsibleState.None
+        : vscode.TreeItemCollapsibleState.Collapsed
+    );
+    this.tooltip = `${run.name} - Status: ${run.status}`;
+    this.description = `version: ${run.version}, status: ${run.status}`;
+    this.children = children;
+  }
+
+  contextValue = 'pipelineRun';
+}
+
+/**
+ * Represents details of a pipeline run in the VS Code tree view.
+ * Displays the stack name for the run, its start time, end time, machine details and Python version.
+ */
+export class PipelineRunTreeItem extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly description: string
+  ) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+    this.tooltip = `${label}: ${description}`;
+  }
+
+  contextValue = 'pipelineRunDetail';
+}
