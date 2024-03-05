c215472b9fc16ee7fdadbb0e07b04622a9613f4c
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-03-04 19:57:02 -0600

    Remove auto-refresh/polling of server status and add reset/reactivate functions

--- src/services/ServerStatusService.ts
+++ src/services/ServerStatusService.ts
@@ -1,37 +1,71 @@
-import { Shell } from "../utils/Shell";
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.See the License for the specific language governing
+// permissions and limitations under the License.
 import { ServerStatus } from "../types/ServerTypes";
+import { ZenMLClient } from './ZenMLClient';
+
+export const INITIAL_ZENML_SERVER_STATUS: ServerStatus = {
+  isConnected: false,
+  url: '',
+  id: '',
+  version: '',
+  debug: false,
+  deployment_type: '',
+  database_type: '',
+  secrets_store_type: '',
+  auth_scheme: '',
+};
 
 export class ServerStatusService {
+  private isActive = true;
+  private apiClient: ZenMLClient = ZenMLClient.getInstance();
   private static instance: ServerStatusService;
-  private shell: Shell;
-  private currentStatus: ServerStatus = { isConnected: false };
+  private currentStatus = { ...INITIAL_ZENML_SERVER_STATUS };
   private listeners: ((status: ServerStatus) => void)[] = [];
 
   /**
-   * Creates an instance of the ServerStatusService class.
-   * This constructor is private to enforce the singleton pattern, ensuring that only one instance of the ServerStatusService exists within the application.
-   * It initializes the service, sets up the shell environment for command execution, and starts polling the ZenML server status.
-   * 
-   * @param {Shell} shell An instance of Shell that provides access to the shell environment to run the script that checks the ZenML server status..
-   */
-  private constructor(shell: Shell) {
-    this.shell = shell;
+ * Private constructor to enforce singleton pattern. Initializes the service by starting to poll the server status from the Flask service.
+ */
+  private constructor() {
+    this.isActive = true;
     this.pollServerStatus();
   }
 
   /**
-   * Retrieves the singleton instance of the ServerStatusService class.
-   * If the instance does not exist, it creates one using the provided Shell instance. 
-   * This ensures that the same instance is used throughout the application to maintain a consistent state.
-   * 
-   * @param {Shell} shell An instance of Shell required to create the ServerStatusService if it does not already exist.
-   * @returns {ServerStatusService} The singleton instance of the ServerStatusService.
+   * Retrieves or creates the singleton instance of the ServerStatusService, ensuring consistent state throughout the application.
+   *
+   * @returns {ServerStatusService} The singleton ServerStatusService instance.
    */
-  public static getInstance(shell: Shell): ServerStatusService {
+  public static getInstance(): ServerStatusService {
     if (!ServerStatusService.instance) {
-      ServerStatusService.instance = new ServerStatusService(shell);
+      ServerStatusService.instance = new ServerStatusService();
     }
     return ServerStatusService.instance;
+  }
+
+  /**
+ * Reactivates the tree view data by setting isActive to true.
+ */
+  public resetStatus() {
+    this.isActive = false;
+    this.currentStatus = { ...INITIAL_ZENML_SERVER_STATUS };
+    this.notifyListeners();
+  }
+
+  /**
+   * Reactivates the tree view data by setting isActive to true.
+   */
+  public reactivate(): void {
+    this.isActive = true;
   }
 
   /**
@@ -55,31 +89,30 @@ export class ServerStatusService {
   }
 
   /**
-   * Asynchronously checks the ZenML server status by executing a Python script through the shell.
-   * This method attempts to gather detailed information about the ZenML server's status, including its connectivity, host, port, store type, and store URL.
-   * In case of an error, it logs the error and returns a status indicating the server is not connected.
-   * 
+   * Asynchronously polls the Flask service for the current ZenML server status, updating connectivity and server details.
+   * In case of an error, it defaults to a disconnected status.
+   *
    * @returns {Promise<ServerStatus>} A promise that resolves to the status of the ZenML server, based on the output of the Python script.
    */
   private async checkZenMLServerStatus(): Promise<ServerStatus> {
     try {
-      const output = await this.shell.runPythonScript("check_server_status.py");
-      try {
-        const serverStatusInfo = JSON.parse(output);
-        return {
-          isConnected: serverStatusInfo.is_connected,
-          host: serverStatusInfo.host,
-          port: serverStatusInfo.port,
-          storeType: serverStatusInfo.store_type,
-          storeUrl: serverStatusInfo.store_url,
-        };
-      } catch (jsonError) {
-        console.error(`Failed to parse JSON from ZenML server status check: ${jsonError}`);
-        return { isConnected: false };
-      }
+      const serverStatusInfo = await this.apiClient.request('get', '/info');
+      const isConnected = serverStatusInfo.deployment_type === 'cloud';
+
+      return {
+        isConnected,
+        url: this.apiClient.getZenMLServerUrl(),
+        id: serverStatusInfo.id,
+        version: serverStatusInfo.version,
+        debug: serverStatusInfo.debug,
+        deployment_type: serverStatusInfo.deployment_type,
+        database_type: serverStatusInfo.database_type,
+        secrets_store_type: serverStatusInfo.secrets_store_type,
+        auth_scheme: serverStatusInfo.auth_scheme,
+      };
     } catch (error) {
       console.error(`Failed to check ZenML server status: ${error}`);
-      return { isConnected: false };
+      return { ...INITIAL_ZENML_SERVER_STATUS };
     }
   }
 
@@ -89,22 +122,29 @@ export class ServerStatusService {
    * This method uses a recursive timeout to ensure continuous polling without stacking calls.
    */
   private async pollServerStatus() {
+    if (!this.isActive) {
+      return;
+    }
+
     try {
       const status = await this.checkZenMLServerStatus();
       if (
         this.currentStatus.isConnected !== status.isConnected ||
-        this.currentStatus.host !== status.host ||
-        this.currentStatus.port !== status.port ||
-        this.currentStatus.storeType !== status.storeType ||
-        this.currentStatus.storeUrl !== status.storeUrl
+        this.currentStatus.url !== status.url ||
+        this.currentStatus.id !== status.id ||
+        this.currentStatus.version !== status.version ||
+        this.currentStatus.debug !== status.debug ||
+        this.currentStatus.deployment_type !== status.deployment_type ||
+        this.currentStatus.database_type !== status.database_type ||
+        this.currentStatus.secrets_store_type !== status.secrets_store_type ||
+        this.currentStatus.auth_scheme !== status.auth_scheme
       ) {
         this.currentStatus = status;
         this.notifyListeners();
       }
     } catch (error) {
-      console.error("Error checking ZenML server status:", error);
+      console.error('Error checking ZenML server status:', error);
     }
-    setTimeout(() => this.pollServerStatus(), 30000);
   }
 
   /**
