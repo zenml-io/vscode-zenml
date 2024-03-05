4782c0367960f03cbfbf6594a247ec8bf0665118
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-03-04 20:00:27 -0600

    Update status bar to reflect changes to the getActiveStack function

--- src/views/statusBar/index.ts
+++ src/views/statusBar/index.ts
@@ -1,60 +1,49 @@
-import * as vscode from "vscode";
-import { getActiveStack } from "../../commands/stack/utils";
-import { Shell } from "../../utils/Shell";
-import { ServerStatusService } from "../../services/ServerStatusService";
+import * as vscode from 'vscode';
+import { getActiveStack } from '../../commands/stack/utils';
+import { ServerStatusService } from '../../services/ServerStatusService';
+import { ZenMLClient } from '../../services/ZenMLClient';
 
 /**
  * Represents the ZenML extension's status bar.
  * This class manages two main status indicators: the server status and the active stack name.
  */
 export default class ZenMLStatusBar {
-  private shell: Shell;
   private static instance: ZenMLStatusBar;
+  private context: vscode.ExtensionContext;
+  private apiClient: ZenMLClient = ZenMLClient.getInstance();
   private serverStatusItem: vscode.StatusBarItem;
   private activeStackItem: vscode.StatusBarItem;
   private serverStatusService: ServerStatusService;
-  private activeStack: string = "Loading...";
+  private activeStack: string = 'Loading...';
 
   /**
    * Initializes a new instance of the ZenMLStatusBar class.
    * Sets up the status bar items for server status and active stack, subscribes to server status updates,
    * and initiates the initial refresh of the status bar state.
-   * 
-   * @param {Shell} shell An instance of Shell used for command execution and interaction with the system shell.
    */
-  constructor(shell: Shell) {
-    this.shell = shell;
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+
     this.serverStatusItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
     this.activeStackItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 99);
-    this.serverStatusService = ServerStatusService.getInstance(this.shell);
-    this.serverStatusService.subscribe((status) => {
-      this.updateServerStatusIndicator(status.isConnected, `${status.host}:${status.port}`);
+    this.serverStatusService = ServerStatusService.getInstance();
+    this.serverStatusService.subscribe(status => {
+      this.updateServerStatusIndicator(status.isConnected, `${this.apiClient.getZenMLServerUrl()}`);
     });
-    this.refreshStatusBarState();
+    this.refreshActiveStack();
   }
 
   /**
    * Retrieves or creates an instance of the ZenMLStatusBar.
    * This method implements the Singleton pattern to ensure that only one instance of ZenMLStatusBar exists.
-   * 
-   * @param {Shell} shell An instance of Shell required to instantiate the ZenMLStatusBar.
+   *
    * @returns {ZenMLStatusBar} The singleton instance of the ZenMLStatusBar.
    */
-  public static getInstance(shell: Shell): ZenMLStatusBar {
+  public static getInstance(context: vscode.ExtensionContext): ZenMLStatusBar {
     if (!ZenMLStatusBar.instance) {
-      ZenMLStatusBar.instance = new ZenMLStatusBar(shell);
+      ZenMLStatusBar.instance = new ZenMLStatusBar(context);
     }
     return ZenMLStatusBar.instance;
-  }
-
-  /**
-   * Refreshes the status bar state by updating the active stack and setting up a periodic refresh every 30 seconds.
-   */
-  private refreshStatusBarState() {
-    this.refreshActiveStack();
-    setInterval(() => {
-      this.refreshActiveStack();
-    }, 30000);
   }
 
   /**
@@ -64,12 +53,12 @@ export default class ZenMLStatusBar {
    */
   public async refreshActiveStack() {
     try {
-      const activeStackName = await getActiveStack(this.shell);
-      this.activeStack = activeStackName;
+      const activeStack = await getActiveStack();
+      this.activeStack = activeStack?.name || 'default';
       this.refreshActiveStackText();
     } catch (error) {
-      console.error("Failed to fetch active ZenML stack:", error);
-      this.activeStack = "Error";
+      console.error('Failed to fetch active ZenML stack:', error);
+      this.activeStack = 'Error';
     }
     this.refreshActiveStackText();
   }
@@ -77,14 +66,16 @@ export default class ZenMLStatusBar {
   /**
    * Updates the server status indicator in the status bar.
    * Sets the text, color, and tooltip of the server status item based on the connection status.
-   * 
+   *
    * @param {boolean} isConnected Whether the server is currently connected.
    * @param {string} serverAddress The address of the server, used in the tooltip.
    */
   private updateServerStatusIndicator(isConnected: boolean, serverAddress: string) {
     this.serverStatusItem.text = isConnected ? `$(vm-active)` : `$(vm-connect)`;
-    this.serverStatusItem.color = isConnected ? "green" : "";
-    this.serverStatusItem.tooltip = isConnected ? `Server running at ${serverAddress}. Click to refresh status.` : "Server not running. Click to refresh status.";
+    this.serverStatusItem.color = isConnected ? 'green' : '';
+    this.serverStatusItem.tooltip = isConnected
+      ? `Server running at ${serverAddress}.`
+      : 'Server not running. Click to refresh status.';
     this.serverStatusItem.show();
   }
 
@@ -94,7 +85,7 @@ export default class ZenMLStatusBar {
    */
   private refreshActiveStackText() {
     this.activeStackItem.text = `${this.activeStack}`;
-    this.activeStackItem.tooltip = "Click to refresh the active ZenML stack";
+    this.activeStackItem.tooltip = 'Active ZenML stack.';
     this.activeStackItem.show();
   }
 }
