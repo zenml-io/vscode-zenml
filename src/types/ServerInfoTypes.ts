6191ead1b0b301ea8a2af24228f940f7f3333f34
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-03-17 23:09:50 -0500

    Add type definitions for all components and responses from the lsp server

---
+++ src/types/ServerInfoTypes.ts
@@ -0,0 +1,98 @@
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.See the License for the specific language governing
+// permissions and limitations under the License.
+export interface ServerStatus {
+  isConnected: boolean;
+  url: string;
+  version: string;
+  store_type: string;
+  deployment_type: string;
+  database_type: string;
+  secrets_store_type: string;
+  database?: string;
+  backup_directory?: string;
+  backup_strategy?: string;
+  auth_scheme?: string;
+  debug?: boolean;
+  id?: string;
+  username?: string | null;
+}
+
+
+/************************************************************************************************
+ * This is the object returned by the @LSP_SERVER.command(zenml.serverInfo")
+ ************************************************************************************************/
+export interface ZenServerDetails {
+  storeInfo: ZenServerStoreInfo;
+  storeConfig: ZenServerStoreConfig;
+}
+
+
+/************************************************************************************************
+ * This is the response from the `zen_store.get_store_info()` method in the ZenML Client.
+ ************************************************************************************************/
+export interface ZenServerStoreInfo {
+  id: string;
+  version: string;
+  debug: boolean;
+  deployment_type: string;
+  database_type: string;
+  secrets_store_type: string;
+  auth_scheme: string;
+  base_url?: string;
+  metadata?: any;
+}
+
+/************************************************************************************************
+ * This is the response from the `zen_store.get_store_config()` method in the ZenML Client.
+ ************************************************************************************************/
+export type ZenServerStoreConfig = RestZenServerStoreConfig | SQLZenServerStoreConfig;
+
+/************************************************************************************************
+ * REST Zen Server Store Config (type === 'rest')
+ ************************************************************************************************/
+export interface RestZenServerStoreConfig {
+  type: string;
+  url: string;
+  secrets_store: any;
+  backup_secrets_store: any;
+  username: string | null;
+  password: string | null;
+  api_key: any;
+  api_token?: string;
+  verify_ssl: boolean;
+  http_timeout: number;
+}
+
+/************************************************************************************************
+ * SQL Zen Server Store Config (type === 'sql')
+ ************************************************************************************************/
+export interface SQLZenServerStoreConfig {
+  type: string;
+  url: string;
+  secrets_store: any;
+  backup_secrets_store: any;
+  driver: string;
+  database: string;
+  username: string | null;
+  password: string | null;
+  ssl_ca: string | null;
+  ssl_cert: string | null;
+  ssl_key: string | null;
+  ssl_verify_server_cert: boolean;
+  pool_size: number;
+  max_overflow: number;
+  pool_pre_ping: boolean;
+  backup_strategy: string;
+  backup_directory: string;
+  backup_database: string | null;
+}
