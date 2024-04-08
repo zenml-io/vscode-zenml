c01f31142c5d52af3d0ec88b2e36e1f54c6f1ad4
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-04-08 13:46:18 -0500

    Update format/lint scripts, and add separate GH Action for linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -15,10 +15,6 @@ ruff bundled/tool || { echo "Linting Python files failed"; exit 1; }
 echo "Type checking Python files with mypy..."
 mypy bundled/tool || { echo "Type checking Python files with mypy failed"; exit 1; }
 
-# Lint TypeScript files with eslint
-echo "Linting TypeScript files..."
-npx eslint 'src/**/*.ts' || { echo "Linting TypeScript files failed"; exit 1; }
-
 # Lint YAML files with yamlfix
 echo "Checking YAML files with yamlfix..."
 yamlfix .github/workflows/*.yml --check || { echo "Linting YAML files failed"; exit 1; }
