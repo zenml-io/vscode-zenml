376b63470a8605ec6428f46929d9b6de24d1b18b
Christopher Perkins <Christopher.Perkins@null.net>
2024-07-11 14:29:10 -0400

    feat: Dag Node onclick goes to dashboard
    Co-authored-by: Erik Wiens <ErikWiens@users.noreply.github.com>

--- resources/dag-view/dag.js
+++ resources/dag-view/dag.js
@@ -1,50 +1,71 @@
-const dag = document.querySelector('#dag');
-const panZoom = svgPanZoom(dag);
-panZoom.enableControlIcons();
-panZoom.setMaxZoom(40);
+(() => {
+  const dag = document.querySelector('#dag');
+  const panZoom = svgPanZoom(dag);
+  panZoom.enableControlIcons();
+  panZoom.setMaxZoom(40);
 
-const resize = () => {
-  dag.setAttribute('width', String(window.innerWidth * 0.95) + 'px');
-  dag.setAttribute('height', String(window.innerHeight * 0.95) + 'px');
-  panZoom.resize();
-  panZoom.fit();
-  panZoom.center();
-};
+  const vscode = acquireVsCodeApi();
 
-resize();
-window.addEventListener('resize', resize);
+  const resize = () => {
+    dag.setAttribute('width', String(window.innerWidth * 0.95) + 'px');
+    dag.setAttribute('height', String(window.innerHeight * 0.95) + 'px');
+    panZoom.resize();
+    panZoom.fit();
+    panZoom.center();
+  };
 
-const edges = [...document.querySelectorAll('polyline')];
-dag.addEventListener('mouseover', evt => {
-  const target = evt.target;
-  const parent = evt.target.parentElement;
+  resize();
+  window.addEventListener('resize', resize);
 
-  if (!parent.classList.contains('node')) {
-    return;
+  const edges = [...document.querySelectorAll('polyline')];
+
+  dag.addEventListener('mouseover', evt => {
+    let target = evt.target;
+    const parent = evt.target.closest('.node');
+
+    if (!parent || target === parent) {
+      return;
+    }
+
+    if (target.tag !== 'div') {
+      target = target.closest('div');
+    }
+
+    const id = parent.dataset.id;
+    const edgesToHighlight = edges.filter(edge => edge.dataset.from === id);
+    target.classList.add('highlight');
+    edgesToHighlight.forEach(edge => edge.classList.add('highlight'));
+  });
+
+  dag.addEventListener('click', evt => {
+    const stepId = evt.target.closest('[data-stepid]')?.dataset.stepid;
+    const artifactId = evt.target.closest('[data-artifactid]')?.dataset.artifactid;
+
+    if (stepId) {
+      vscode.postMessage({ command: 'step', id: stepId });
+    }
+
+    if (artifactId) {
+      vscode.postMessage({ command: 'artifact', id: artifactId });
+    }
+  });
+
+  const nodes = [...document.querySelectorAll('.node > div')];
+
+  nodes.forEach(node => {
+    const id = node.parentElement.dataset.id;
+
+    node.addEventListener('mouseleave', () => {
+      const edgesToHighlight = edges.filter(edge => edge.dataset.from === id);
+      node.classList.remove('highlight');
+      edgesToHighlight.forEach(edge => edge.classList.remove('highlight'));
+    });
+  });
+
+  function update() {
+    vscode.postMessage({ command: 'update' });
   }
 
-  const id = parent.dataset.id;
-  const edgesToHighlight = edges.filter(edge => edge.dataset.from === id);
-  target.classList.add('highlight');
-  edgesToHighlight.forEach(edge => edge.classList.add('highlight'));
-});
-
-const nodes = [...document.querySelectorAll('.node > div')];
-
-nodes.forEach(node => {
-  const id = node.parentElement.dataset.id;
-
-  node.addEventListener('mouseleave', () => {
-    const edgesToHighlight = edges.filter(edge => edge.dataset.from === id);
-    node.classList.remove('highlight');
-    edgesToHighlight.forEach(edge => edge.classList.remove('highlight'));
-  });
-});
-
-const vscode = acquireVsCodeApi();
-function update() {
-  vscode.postMessage({ command: 'update' });
-}
-
-const button = document.querySelector('#update button');
-button?.addEventListener('click', update);
+  const button = document.querySelector('#update button');
+  button?.addEventListener('click', update);
+})();
