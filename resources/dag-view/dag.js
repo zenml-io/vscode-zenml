a17751e68ddfde57b3dab2cee763c34f00af5010
Christopher Perkins <Christopher.Perkins@null.net>
2024-07-11 12:46:20 -0400

    feat: Changed update bar to title/update bar
    Co-authored-by: Erik Wiens <ErikWiens@users.noreply.github.com>

--- resources/dag-view/dag.js
+++ resources/dag-view/dag.js
@@ -5,7 +5,7 @@ panZoom.setMaxZoom(40);
 
 const resize = () => {
   dag.setAttribute('width', String(window.innerWidth * 0.95) + 'px');
-  dag.setAttribute('height', String(window.innerHeight * 0.94) + 'px');
+  dag.setAttribute('height', String(window.innerHeight * 0.95) + 'px');
   panZoom.resize();
   panZoom.fit();
   panZoom.center();
@@ -40,3 +40,11 @@ nodes.forEach(node => {
     edgesToHighlight.forEach(edge => edge.classList.remove('highlight'));
   });
 });
+
+const vscode = acquireVsCodeApi();
+function update() {
+  vscode.postMessage({ command: 'update' });
+}
+
+const button = document.querySelector('#update button');
+button?.addEventListener('click', update);
