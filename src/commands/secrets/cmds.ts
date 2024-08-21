94b0f73130c10e218bc0ebb9fd3fbb33de832e77
Alex Sklar <sklarfox@gmail.com>
2024-08-21 12:25:34 -0700

    feat(secrets): add commands to register, update, and remove OpenAI API keys with the VSCode SecretsStorage API

---
+++ src/commands/secrets/cmds.ts
@@ -0,0 +1,56 @@
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.See the License for the specific language governing
+// permissions and limitations under the License.
+
+import * as vscode from 'vscode';
+import type { ExtensionContext } from 'vscode';
+
+// TODO I don't think retrieval of an api key will live in here
+
+const registerOpenAIAPIKey = async (context: ExtensionContext) => {
+  let apiKey = await context.secrets.get('OPENAI_API_KEY');
+
+  if (apiKey) {
+    apiKey = await vscode.window.showInputBox({
+      prompt: 'OpenAI API Key already exists, enter a new value to update.',
+      password: true,
+    });
+  } else {
+    apiKey = await vscode.window.showInputBox({
+      prompt: 'Please enter your OpenAI API key',
+      password: true,
+    });
+  }
+
+  if (apiKey === undefined) {
+    return undefined;
+  }
+
+  await context.secrets.store('OPENAI_API_KEY', apiKey);
+  vscode.window.showInformationMessage('OpenAI API key stored successfully.');
+};
+
+const deleteOpenAIAPIKey = async (context: ExtensionContext) => {
+  const apiKey = await context.secrets.get('OPENAI_API_KEY');
+
+  if (apiKey === undefined) {
+    vscode.window.showInformationMessage('No OpenAI API key exists.');
+    return;
+  }
+  await context.secrets.delete('OPENAI_API_KEY');
+  vscode.window.showInformationMessage('OpenAI API key successfully removed.');
+};
+
+export const secretsCommands = {
+  registerOpenAIAPIKey,
+  deleteOpenAIAPIKey,
+};
---
+++ src/commands/secrets/registry.ts
@@ -0,0 +1,50 @@
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.See the License for the specific language governing
+// permissions and limitations under the License.
+
+// TODO the registration of secrets commands should go in here, which is then imported into the ZenExtension file
+// In registry, the "register command thing is passed context (i.e. context.secrets) as an arg"
+
+import { secretsCommands } from './cmds';
+import { registerCommand } from '../../common/vscodeapi';
+import { ZenExtension } from '../../services/ZenExtension';
+import { ExtensionContext, commands } from 'vscode';
+
+/**
+ * Registers pipeline-related commands for the extension.
+ *
+ * @param {ExtensionContext} context - The context in which the extension operates, used for registering commands and managing their lifecycle.
+ */
+export const registerSecretsCommands = (context: ExtensionContext) => {
+  try {
+    const registeredCommands = [
+      registerCommand(
+        'zenml.registerOpenAIAPIKey',
+        async () => await secretsCommands.registerOpenAIAPIKey(context)
+      ),
+      registerCommand(
+        'zenml.deleteOpenAIAPIKey',
+        async () => await secretsCommands.deleteOpenAIAPIKey(context)
+      ),
+    ];
+
+    registeredCommands.forEach(cmd => {
+      context.subscriptions.push(cmd);
+      ZenExtension.commandDisposables.push(cmd);
+    });
+
+    commands.executeCommand('setContext', 'secretsCommandsRegistered', true);
+  } catch (error) {
+    console.error('Error registering pipeline commands:', error);
+    commands.executeCommand('setContext', 'secretsCommandsRegistered', false);
+  }
+};
