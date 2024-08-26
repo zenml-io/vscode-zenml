72ca4ceceee73db4e27901ebe91a62611fd8d9f8
Nathaniel Xu <nxu981@gmail.com>
2024-08-26 14:39:24 -0400

    chore: removed unnecessary function

--- src/views/panel/panelView/PanelDataProvider.ts
+++ src/views/panel/panelView/PanelDataProvider.ts
@@ -100,8 +100,4 @@ export class PanelDataProvider implements TreeDataProvider<TreeItem> {
 
     return [new PanelTreeItem(this.dataType, this.data)];
   }
-
-  public getData(): TreeItem | JsonObject {
-    return this.data
-  }
 }
