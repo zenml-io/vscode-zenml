05611468f219c4579235b306b7c7ffa85bc73a97
Alan Cho <alancho397@gmail.com>
2024-08-26 17:27:07 -0700

    feat: Buttons are added to the UI to select context

--- resources/chat-view/chat.js
+++ resources/chat-view/chat.js
@@ -1,36 +1,35 @@
-(function() {
-    const vscode = acquireVsCodeApi();
-  
-    // Function to send the message
-    function sendMessage() {
-        const input = document.getElementById('messageInput');
-        if (input.value.trim()) {
-            const message = input.value;
-            input.value = ''; // Clear input
-  
-            // Post the message back to the extension
-            vscode.postMessage({
-                command: 'sendMessage',
-                text: message
-            });
-  
-            // Append the message to the chat log
-            const messagesDiv = document.getElementById('messages');
-            const messageDiv = document.createElement('div');
-            messageDiv.className = 'message';
-            messageDiv.innerHTML = marked(message);
-            messagesDiv.appendChild(messageDiv);
-        }
+(function () {
+  const vscode = acquireVsCodeApi();
+
+  // Function to send the message
+  function sendMessage(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const text = formData.get('messageInput').trim();
+    const context = [];
+
+    context.push(formData.get('serverContext'));
+    context.push(formData.get('environmentContext'));
+    context.push(formData.get('pipelineContext'));
+    context.push(formData.get('stackContext'));
+    context.push(formData.get('stackComponentsContext'));
+
+    if (text) {
+      vscode.postMessage({
+        command: 'sendMessage',
+        text: text,
+        context: context,
+      });
+
+      event.target.reset();
+
+      const messagesDiv = document.getElementById('messages');
+      const messageDiv = document.createElement('div');
+      messageDiv.className = 'message';
+      messageDiv.innerHTML = marked(message);
+      messagesDiv.appendChild(messageDiv);
     }
-  
-    // Click event for the send button
-    document.getElementById('sendMessage').addEventListener('click', sendMessage);
-  
-    // Keydown event for the Enter key
-    document.getElementById('messageInput').addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault(); // Prevent default Enter key behavior (e.g., newline)
-            sendMessage();
-        }
-    });
-  })();
+  }
+
+  document.getElementById('chatForm').addEventListener('submit', sendMessage);
+})();
