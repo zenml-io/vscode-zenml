515d65be315a2c2c39c43b0c611f0a6f1945232d
marwan37 <marwan.ext@zenml.io>
2025-03-10 14:32:02 -0500

    maintain no-explicit-any at warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -49,7 +49,7 @@ export default tseslint.config(
         },
       ],
       // Disable rules
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unsafe-function-type': 'warn',
       '@typescript-eslint/consistent-type-definitions': 'off',
       // Prettier rules
