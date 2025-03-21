fc50c5fc4eb28fe7298aa1c97c946337721a3bad
marwan37 <marwan.ext@zenml.io>
2025-03-21 07:28:03 -0500

    add ProjectTypes.ts for zenml library response types

---
+++ src/types/ProjectTypes.ts
@@ -0,0 +1,40 @@
+// Copyright(c) ZenML GmbH 2024. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+/**
+ * Interface representing a ZenML Project.
+ */
+export interface Project {
+  id: string;
+  name: string;
+  display_name?: string;
+  created?: string;
+  updated?: string;
+}
+
+/**
+ * Interface representing a ZenML Projects response with pagination.
+ */
+export interface ProjectsResponse {
+  projects: Project[];
+  total: number;
+  total_pages: number;
+  current_page: number;
+  items_per_page: number;
+}
+
+export interface GetProjectByNameResponse {
+  id: string;
+  name: string;
+  display_name?: string;
+}
