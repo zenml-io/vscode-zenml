61704667b5814d5b6a4ca5c44a6c9287dfaa1f6f
marwan37 <marwan.ext@zenml.io>
2025-03-25 18:09:15 -0500

    remove pipeline run status icons from constants -- handle in ui-constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,7 +10,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing
 // permissions and limitations under the License.
-import { ThemeColor, ThemeIcon } from 'vscode';
 import { ServerStatus } from '../types/ServerInfoTypes';
 
 export const PYTOOL_MODULE = 'zenml-python';
@@ -51,12 +50,4 @@ export const INITIAL_ZENML_SERVER_STATUS: ServerStatus = {
   database_type: '',
   secrets_store_type: '',
   username: null,
-};
-
-export const PIPELINE_RUN_STATUS_ICONS: Record<string, ThemeIcon> = {
-  initializing: new ThemeIcon('loading~spin'),
-  failed: new ThemeIcon('x', new ThemeColor('charts.red')),
-  completed: new ThemeIcon('check', new ThemeColor('charts.green')),
-  running: new ThemeIcon('clock', new ThemeColor('charts.orange')),
-  cached: new ThemeIcon('history', new ThemeColor('charts.yellow')),
 };
